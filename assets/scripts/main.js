85ecf06f7e59dc5cb4ecf3f6d26f9fe6e7f492da
galaxydevel0per <58356708+Galaxy202@users.noreply.github.com>
2023-03-15 22:19:59 +0400

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,32 @@
+//let date = new Date;
+//document.getElementById("year").innerText = date.getFullYear();
+
+// document.getElementById("click-1").onclick = function() {
+//     if (document.getElementById("content-1").style.display == "none") {
+//         document.getElementById("content-1").style.display = "block";
+//     } else {
+//         document.getElementById("content-1").style.display = "none";
+//     }
+// };
+if (new Date().getFullYear() != 2023) { console.error("Time traveler detected! Return to the past. ") }
+function a() {
+    if (document.getElementById("content-1").style.display == "none") {
+    document.getElementById("content-1").style.display = "block";
+} else {
+    document.getElementById("content-1").style.display = "none";
+}
+}
+function b() {
+    if (document.getElementById("content-2").style.display == "none") {
+    document.getElementById("content-2").style.display = "block";
+} else {
+    document.getElementById("content-2").style.display = "none";
+}
+}
+function c() {
+    if (document.getElementById("content-3").style.display == "none") {
+    document.getElementById("content-3").style.display = "block";
+} else {
+    document.getElementById("content-3").style.display = "none";
+}
+}
